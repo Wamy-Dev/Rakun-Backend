89bf7dda8075b121a6f570cb7b5fd1f27f9d56b9
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-21 18:49:59 -0500

    basic returning

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functions.statusFunctions import getSearchStatus, getWebsiteStatus
 from typing import Literal
 from api_analytics.fastapi import Analytics
 from functions.getFunctions import getByMalId
+from fastapi import HTTPException
 
 app.add_middleware(Analytics, api_key='54494a91-1dee-4eb9-86d1-709d0971bec5')
 
@@ -18,21 +19,21 @@ def get_status():
 def get_by_mal_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], mal_id: int):
     data = getByMalId(collection, mal_id)
     if data is None:
-        return {
+        raise HTTPException(status_code=404, detail={
             "status": "NOK",
             "message": "Item not found. Make sure you are using the correct collection and have the correct MAL ID."
-        }
+        })
     elif data == "Error":
-        return {
+        raise HTTPException(status_code=500, detail={
             "status": "NOK",
-            "message": "There was an error retrieving your data. Please try again later."
-        }
+            "message": "Item not found. Make sure you are using the correct collection and have the correct MAL ID."
+        })
     else:
-        return {
+        raise HTTPException(status_code=200, detail={
             "status": "OK",
+            "message": "Successfully got item by MAL ID.",
             "data": data
-        }
-
+        })
 @app.get("/get/{collection}/{anilist_id}" , tags=["Items"], summary="Get an item by its ANILIST ID. Make sure to use the correct collection.")
 def get_by_anilist_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], anilist_id: int):
     print(collection, anilist_id)
