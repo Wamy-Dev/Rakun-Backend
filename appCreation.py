deb73e6fe99cf84170f5e9973cb50cecf1fd14af
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-21 18:33:46 -0500

    basic endpoints

--- appCreation.py
+++ appCreation.py
@@ -8,7 +8,25 @@ def createMongo():
 mongoClient = createMongo()
 
 def createFastAPI():
-    app = FastAPI()
+    tags_metadata = [
+        {
+            "name": "Items",
+            "description": "Items are the main functions of Rakun. They are the things you can search for. They contain metadata as well as the links you want.",
+        },
+        {
+            "name": "Basics",
+            "description": "These are the basic endpoints of the API. General endpoints.",
+        }
+    ]
+    app = FastAPI(
+        title="Rakun API",
+        description='''
+        This is the API for Rakun.
+
+        You can find Rakun at https://rakun.app and https://search.rakun.app
+        ''',
+        openapi_tags=tags_metadata
+    )
     return app
 
 fastAPI = createFastAPI()
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from appCreation import fastAPI as app
 from functions.statusFunctions import getSearchStatus, getWebsiteStatus
+from typing import Literal
 
-@app.get("/status")
+@app.get("/status", tags=["Basics"])
 def get_status():
     return {
         "search": getSearchStatus(),
         "website": getWebsiteStatus(),
         "api": "OK"
     }
+@app.get("/get/{collection}/{mal_id}" , tags=["Items"], summary="Get an item by its MAL ID. Make sure to use the correct collection.")
+def get_by_mal_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], mal_id: int):
+    print(collection, mal_id)
 
-    
+@app.get("/get/{collection}/{anilist_id}" , tags=["Items"], summary="Get an item by its ANILIST ID. Make sure to use the correct collection.")
+def get_by_anilist_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], anilist_id: int):
+    print(collection, anilist_id)
+
+@app.get("/get/{collection}/{id}" , tags=["Items"], summary="Get an item by its hashed ID. Make sure to use the correct collection.", description="The ID is the hashed ID in md5 of the MAL name of the item + the type of the item (title-cased). For example the ID of the anime 'Boku no Hero Academia' is 'Boku no Hero AcademiaAnime' then hashed to md5 gives this id -> `2bfbcac5fc33577c29656bd102c2db1a`. This likely is not useful for you, but it is how to search engine shows the pages, and is the url for the page.")
+def get_by_hashed_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], id: str):
+    print(collection, id)
+
