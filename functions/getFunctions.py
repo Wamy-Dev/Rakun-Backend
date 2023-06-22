710ecec9ecd3a7a4dc0601c340950171a8053846
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-21 19:20:08 -0500

    hashed id and anilist id

--- functions/getFunctions.py
+++ functions/getFunctions.py
@@ -14,6 +14,32 @@ def getByMalId(collection, id):
         print(e)
         return "Error"
 
-
+def getByAnilistId(collection, id):
+    try:
+        db = mongoClient["scraper"]
+        dbCollection = db[collection.capitalize()]
+        item = dbCollection.find_one({"ani_id": id})
+        if item is None:
+            return None
+        else:
+            item.pop("_id")
+            return item
+    except Exception as e:
+        print(e)
+        return "Error"
+        
+def getByHashedId(collection, id):
+    try:
+        db = mongoClient["scraper"]
+        dbCollection = db[collection.capitalize()]
+        item = dbCollection.find_one({"id": id})
+        if item is None:
+            return None
+        else:
+            item.pop("_id")
+            return item
+    except Exception as e:
+        print(e)
+        return "Error"
 
 
--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from appCreation import fastAPI as app
 from functions.statusFunctions import getSearchStatus, getWebsiteStatus
 from typing import Literal
 from api_analytics.fastapi import Analytics
-from functions.getFunctions import getByMalId
+from functions.getFunctions import getByMalId, getByAnilistId, getByHashedId
 from fastapi import HTTPException
-from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import FileResponse
 app.add_middleware(Analytics, api_key='54494a91-1dee-4eb9-86d1-709d0971bec5')
-app.mount("", StaticFiles(directory="static"), name="static")
 
 @app.get("/favicon.ico", tags=["Basics"])
 def favicon():
-    return app.get_static_file("favicon.ico")
+    return FileResponse("static/favicon.ico")
+
 @app.get("/status", tags=["Basics"])
 def get_status():
     return {
@@ -20,7 +19,7 @@ def get_status():
         "api": "OK"
     }
 
-@app.get("/get/{collection}/{mal_id}" , tags=["Items"], summary="Get an item by its MAL ID. Make sure to use the correct collection.")
+@app.get("/get/mal/{collection}/{mal_id}" , tags=["Items"], summary="Get an item by its MAL ID. Make sure to use the correct collection.")
 def get_by_mal_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], mal_id: int):
     data = getByMalId(collection, mal_id)
     if data is None:
@@ -39,11 +38,44 @@ def get_by_mal_id(collection: Literal["anime", "eroanime", "manga", "eromanga"],
             "message": "Successfully got item by MAL ID.",
             "data": data
         })
-@app.get("/get/{collection}/{anilist_id}" , tags=["Items"], summary="Get an item by its ANILIST ID. Make sure to use the correct collection.")
-def get_by_anilist_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], anilist_id: int):
-    print(collection, anilist_id)
 
-@app.get("/get/{collection}/{id}" , tags=["Items"], summary="Get an item by its hashed ID. Make sure to use the correct collection.", description="The ID is the hashed ID in md5 of the MAL name of the item + the type of the item (title-cased). For example the ID of the anime 'Boku no Hero Academia' is 'Boku no Hero AcademiaAnime' then hashed to md5 gives this id -> `2bfbcac5fc33577c29656bd102c2db1a`. This likely is not useful for you, but it is how to search engine shows the pages, and is the url for the page.")
+@app.get("/get/anilist/{collection}/{anilist_id}" , tags=["Items"], summary="Get an item by its ANILIST ID. Make sure to use the correct collection.")
+def get_by_anilist_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], anilist_id: int):
+    data = getByAnilistId(collection, anilist_id)
+    if data is None:
+        raise HTTPException(status_code=404, detail={
+            "status": "NOK",
+            "message": "Item not found. Make sure you are using the correct collection and have the correct ANILIST ID."
+        })
+    elif data == "Error":
+        raise HTTPException(status_code=500, detail={
+            "status": "NOK",
+            "message": "Item not found. Make sure you are using the correct collection and have the correct ANILIST ID."
+        })
+    else:
+        raise HTTPException(status_code=200, detail={
+            "status": "OK",
+            "message": "Successfully got item by ANILIST ID.",
+            "data": data
+        })
+
+@app.get("/get/id/{collection}/{id}" , tags=["Items"], summary="Get an item by its hashed ID. Make sure to use the correct collection.", description="The ID is the hashed ID in md5 of the MAL name of the item + the type of the item (title-cased). For example the ID of the anime `Boku no Hero Academia` is `Boku no Hero AcademiaAnime` then hashed to md5 gives this id -> `2bfbcac5fc33577c29656bd102c2db1a`. This likely is not useful for you, but it is how to search engine shows the pages, and is the static url for the page.")
 def get_by_hashed_id(collection: Literal["anime", "eroanime", "manga", "eromanga"], id: str):
-    print(collection, id)
+    data = getByHashedId(collection, id)
+    if data is None:
+        raise HTTPException(status_code=404, detail={
+            "status": "NOK",
+            "message": "Item not found. Make sure you are using the correct collection and have the correct HASHED ID."
+        })
+    elif data == "Error":
+        raise HTTPException(status_code=500, detail={
+            "status": "NOK",
+            "message": "Item not found. Make sure you are using the correct collection and have the correct HASHED ID."
+        })
+    else:
+        raise HTTPException(status_code=200, detail={
+            "status": "OK",
+            "message": "Successfully got item by HASHED ID.",
+            "data": data
+        })
 
