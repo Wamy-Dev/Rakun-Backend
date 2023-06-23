992c408ea286fa5210291cbf7d77d642774c3bf1
Wamy-Dev <25761442+Wamy-Dev@users.noreply.github.com>
2023-06-22 22:24:38 -0500

    hotfix for status error when 500

--- functions/statusFunctions.py
+++ functions/statusFunctions.py
@@ -1,14 +1,20 @@
 import requests
 
 def getSearchStatus():
-    req = requests.get('https://search.rakun.app/health')
-    if req.status_code == 200:
-        return "OK"
-    else:
+    try:
+        req = requests.get('https://search.rakun.app/health')
+        if req.status_code == 200:
+            return "OK"
+        else:
+            return "NOK"
+    except:
         return "NOK"
 def getWebsiteStatus():
-    req = requests.get('https://rakun.app')
-    if req.status_code == 200:
-        return "OK"   
-    else:
+    try:
+        req = requests.get('https://rakun.app')
+        if req.status_code == 200:
+            return "OK"   
+        else:
+            return "NOK"
+    except:
         return "NOK"
